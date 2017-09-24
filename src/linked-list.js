5e6543a44bc61d2ea3b5f5fa95fc176018ec6a70
Lizaveta  Shirokova <vardomskaja@rambler.ru>
2017-09-24 19:20:53 +0300

    double linked list

--- src/linked-list.js
+++ src/linked-list.js
@@ -19,63 +19,87 @@ class LinkedList {
             this._tail = NewNode;
         }
         this.length++;
+        return this;
     }
 
     head() {
-        return this._head.data;
+        if (this._head) return this._head.data;
+        else return null;
     }
 
     tail() {
-        return this._tail.data;
+        if (this._tail) 
+            return this._tail.data;
+        else return null;
     }
 
-    at(index) {
+    nodeAt(index){
         if (index > -1 && index < this.length){
-            var CurrentNode = this._head,
-            i = 0;
-            while (i++ < index){
+            var CurrentNode = this._head;
+            var i = 0;
+            while (i < index){
                 CurrentNode = CurrentNode.next;
+                i++;
             }
-            return CurrentNode.data;
+            return CurrentNode;
         }
         else{
             return null;
         }
     }
 
-    insertAt(position, data) {
-      
-       
-           
-            
+    at(index) {
+        return this.nodeAt(index).data;
+    }
 
+    insertAt(position, data) {
+        var newNode = new Node(data);
+        var current = this.nodeAt(position);
+
+        if (current !== null) {
+            current.prev.next = newNode;
+            newNode.prev = current.prev;
+            current.prev = newNode;
+            newNode.next = current;
+            this.length++;
+        } else if (this.length == 0 && position == 0){
+            this.append(data);
+        }
+
+        return this;           
     }
 
     isEmpty() {
-        var x = new Boolean(true);
+        var x = true;
         if (this.length === 0){ x = true;}
         if (this.length != 0){ x = false;}
         return x;
     }
 
-    clear() {}
+    clear() {
+        for (var ind = this.length - 1; ind >= 0; ind--){
+            this.deleteAt(ind);
+        }
+        return this;
+    }
 
     deleteAt(index) {
-        
-        if (index > -1 && index < this.length){
+        var current = this.nodeAt(index);
+        if (current !== null) {
             
-             var current = this._head,
-             i = 0;
-                       
-             while(i++ < index){
-                 current = current.next;
+            if (current.prev) current.prev.next = current.next;
+            if (current.next) current.next.prev = current.prev;
+
+            if (current == this._head) {
+                this._head = current.next;
             }
-            
-            //skip over the item to remove
-             current.prev.next = current.next;
+            if (current == this._tail) {
+                this._tail = current.prev;
+            }
+            this.length--;
         }
-        this.length--;
-        return current.data;                  
+        
+        return this;                  
   }
 
     reverse() {
@@ -89,11 +113,20 @@ class LinkedList {
             pointHead.prev = tempor;
             pointHead = pointHead.next;
         }
-        
+        return this;
     }
 
     indexOf(data) {
-       
+        var temp = this._head;
+        for (var i = 0; temp && temp.data != data; i++) {
+            temp = temp.next;
+        }
+
+        if (i == this.length){
+            return -1;
+        }
+        
+       return i;
     }
 }
 
